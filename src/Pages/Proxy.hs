a43ac83250789cff64df44584839bde3031d64f5
Steven Vandevelde <icid.asset@gmail.com>
2016-12-20 00:14:25 +0100

    Add initial loading screen

--- src/Pages/Proxy.hs
+++ src/Pages/Proxy.hs
@@ -1,6 +1,8 @@
 module Pages.Proxy where
 
+import Lucid (renderText)
 import Template
+import qualified Components.LoadingScreen
 import qualified Data.Aeson as Aeson (encode)
 import qualified Data.Text as Text (append, concat)
 import qualified Data.Text.Lazy as LazyText (toStrict)
@@ -12,6 +14,18 @@ template obj _ = mconcat
   [ container_
       [ class_ "elm-container" ] ↩
       []
+
+  , script_
+      []
+      ( Text.concat
+          [ "document.querySelector('.elm-container').innerHTML = '"
+          , obj
+              |> Components.LoadingScreen.template
+              |> renderText
+              |> LazyText.toStrict
+          , "';"
+          ]
+      )
 
   , script_
       [ class_ "config", type_ "application/json" ]
